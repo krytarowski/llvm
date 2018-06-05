50276909bce4d0d89fbeaa40860de61b46f72af0
Yang Zheng <tomsun.0.7@gmail.com>
2018-06-05 21:36:14 +0800

    Adapt cmake files for static linking

--- cmake/modules/AddLLVM.cmake
+++ cmake/modules/AddLLVM.cmake
@@ -774,6 +774,9 @@ macro(add_llvm_executable name)
 endmacro(add_llvm_executable name)
 
 function(export_executable_symbols target)
+  if (LLVM_BUILD_STATIC AND ${CMAKE_SYSTEM_NAME} MATCHES "NetBSD")
+    return()
+  endif()
   if (LLVM_EXPORTED_SYMBOL_FILE)
     # The symbol file should contain the symbols we want the executable to
     # export
